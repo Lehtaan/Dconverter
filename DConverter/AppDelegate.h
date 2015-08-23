66d327f8ac96c26686fd9bb0d3ce610a43dbc907
Lehtaan <l3htaan@gmail.com>
2015-08-23 19:23:35 +0200

    Initial Commit

---
+++ DConverter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DConverter
+//
+//  Created by Janis Böhm on 23/08/15.
+//  Copyright (c) 2015 Janis Böhm. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ DConverter/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  DConverter
+//
+//  Created by Janis Böhm on 23/08/15.
+//  Copyright (c) 2015 Janis Böhm. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
